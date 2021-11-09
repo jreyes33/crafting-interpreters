fa6ed6d58bc81007e8b407614a0968292f9926bd
Jonathan Reyes <j@jreyes.org>
2021-11-09 17:05:46 -0500

    Done with chapter 6 (parsing expressions) in java

--- jlox/src/dev/jona/lox/Lox.java
+++ jlox/src/dev/jona/lox/Lox.java
@@ -45,15 +45,25 @@ public class Lox {
     private static void run(String source) {
         var scanner = new Scanner(source);
         var tokens = scanner.scanTokens();
+        var parser = new Parser(tokens);
+        var expression = parser.parse();
 
-        // For now, just print the tokens.
-        for (var token : tokens) {
-            System.out.println(token);
-        }
+        // Stop if there was a syntax error.
+        if (hadError) return;
+
+        System.out.println(new AstPrinter().print(expression));
     }
 
     static void error(int line, String message) {
         report(line, "", message);
+    }
+
+    static void error(Token token, String message) {
+        if (token.type() == TokenType.EOF) {
+            report(token.line(), " at end", message);
+        } else {
+            report(token.line(), " at '" + token.lexeme() + "'", message);
+        }
     }
 
     private static void report(int line, String where, String message) {
