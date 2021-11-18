86974027bb7894b15fd258d4c174d8e0208ee271
Jonathan Reyes <j@jreyes.org>
2021-11-18 16:51:46 -0500

    Generate all code for visitor pattern using macros

--- jli/src/expr.rs
+++ jli/src/expr.rs
@@ -1,66 +1,53 @@
 use crate::object::Object;
 use crate::token::Token;
 use crate::Result;
+use paste::paste;
 use std::rc::Rc;
 
 pub type VisitorResult = Result<Rc<dyn Object>>;
 
-pub trait Visitor<O> {
-    fn visit_binary_expr(&self, expr: &Binary) -> O;
-    fn visit_grouping_expr(&self, expr: &Grouping) -> O;
-    fn visit_literal_expr(&self, expr: &Literal) -> O;
-    fn visit_unary_expr(&self, expr: &Unary) -> O;
-}
-
-macro_rules! expr {
-    ($v:vis $s:ident($($f:ident : $t:ty),*)) => {
-        $v struct $s {
-            $(pub $f: $t,)*
+macro_rules! ast {
+    ($trait:ident, $($s:ident($($f:ident : $t:ty),*)),*$(,)*) => {
+        pub trait $trait {
+            fn accept(&self, visitor: &dyn Visitor<VisitorResult>) -> VisitorResult;
         }
 
-        impl $s {
-            pub fn boxed($($f: $t,)*) -> Box<Self> {
-                Box::new(Self::new($($f,)*))
-            }
-
-            fn new($($f: $t,)*) -> Self {
-                Self { $($f,)* }
+        pub trait Visitor<O> {
+            paste! {
+                $(fn [<visit_ $s:lower _ $trait:lower>](&self, expr: &$s) -> O;)*
             }
         }
 
+        $(
+            pub struct $s {
+                $(pub $f: $t,)*
+            }
+
+            impl $s {
+                pub fn boxed($($f: $t,)*) -> Box<Self> {
+                    Box::new(Self::new($($f,)*))
+                }
+
+                fn new($($f: $t,)*) -> Self {
+                    Self { $($f,)* }
+                }
+            }
+
+            impl $trait for $s {
+                fn accept(&self, visitor: &dyn Visitor<VisitorResult>) -> VisitorResult {
+                    paste! {
+                        visitor.[<visit_ $s:lower _ $trait:lower>](self)
+                    }
+                }
+            }
+        )*
     }
 }
 
-pub trait Expr {
-    fn accept(&self, visitor: &dyn Visitor<VisitorResult>) -> VisitorResult;
-}
-
-expr!(pub Binary(left: Box<dyn Expr>, operator: Token, right: Box<dyn Expr>));
-expr!(pub Grouping(expression: Box<dyn Expr>));
-expr!(pub Literal(value: Rc<dyn Object>));
-expr!(pub Unary(operator: Token, right: Box<dyn Expr>));
-
-// TODO: is it possible to generate these methods with the macro?
-impl Expr for Binary {
-    fn accept(&self, visitor: &dyn Visitor<VisitorResult>) -> VisitorResult {
-        visitor.visit_binary_expr(self)
-    }
-}
-
-impl Expr for Grouping {
-    fn accept(&self, visitor: &dyn Visitor<VisitorResult>) -> VisitorResult {
-        visitor.visit_grouping_expr(self)
-    }
-}
-
-impl Expr for Literal {
-    fn accept(&self, visitor: &dyn Visitor<VisitorResult>) -> VisitorResult {
-        visitor.visit_literal_expr(self)
-    }
-}
-
-impl Expr for Unary {
-    fn accept(&self, visitor: &dyn Visitor<VisitorResult>) -> VisitorResult {
-        visitor.visit_unary_expr(self)
-    }
-}
+ast!(
+    Expr,
+    Binary(left: Box<dyn Expr>, operator: Token, right: Box<dyn Expr>),
+    Grouping(expression: Box<dyn Expr>),
+    Literal(value: Rc<dyn Object>),
+    Unary(operator: Token, right: Box<dyn Expr>),
+);
